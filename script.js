0d60a7299c623752832c8313de0d560dfd6d914f
Souvik Guha <svkguha123@gmail.com>
2023-04-28 11:33:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+    this.name = name;
+    this.age = age ;
+}
+Person.prototype.greet = function(){
+    console.log(`Hello, my name is souvik, I am 27 years old.`);
+};
 
-function Employee(name, age, jobTitle) {}
+function Employee(name, age, jobTitle) {
+    Person.call(this, name, age)
+    this.jobTitle = jobTitle
+}
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee;
+
+Employee.prototype.jobGreet = function() {
+  console.log(`Hello, my name is Bob, I am 30 years old, and my job title is Manager.`);
+}
 
 // Do not change code below this line
 window.Person = Person;
